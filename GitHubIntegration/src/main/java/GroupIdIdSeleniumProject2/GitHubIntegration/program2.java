93fb132350ea846f5152c27040d984e4ff45516f
neerujain75 <49033008+neerujain75@users.noreply.github.com>
2019-03-29 23:13:12 -0400

    Update program2.java
    Commit from GitHib

--- GitHubIntegration/src/main/java/GroupIdIdSeleniumProject2/GitHubIntegration/program2.java
+++ GitHubIntegration/src/main/java/GroupIdIdSeleniumProject2/GitHubIntegration/program2.java
@@ -5,6 +5,7 @@ public class program2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Check into Git 213");
+		System.out.println("Check into Git Change");
 
 	}
 
